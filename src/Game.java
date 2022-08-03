1d35478639d1748f8d7b0ff1b9a11229c478c307
Andrew Howell <andrew.t.howell@gmail.com>
2022-08-03 20:56:29 -0230

    Added menu method and documents containing words for each difficulty.

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,15 @@
 public class Game {
     public static void main(String[] args) {
-	System.out.println("Hello.");
+	System.out.println("Welcome to Hangman!");
+	printMenu();
+    }
+
+    //Print menu method
+    public static void printMenu(){
+	System.out.println("Select a difficulty:");
+	System.out.println("1 - Easy    [3-4 letter words]");
+	System.out.println("2 - Medium  [5-6 letter words]");
+	System.out.println("3 - Hard    [7-8 letter words]" );
+	System.out.println("0 - Quit");
     }
 }
