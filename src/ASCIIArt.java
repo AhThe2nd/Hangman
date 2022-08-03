3274aad7f95584e2b672e6be5f8d21b4b294996a
Andrew Howell <andrew.t.howell@gmail.com>
2022-08-03 21:21:44 -0230

    - Added word lists  - Created menu method  - Started construction of switch statement to print  the game board based on number of strikes

--- src/ASCIIArt.java
+++ src/ASCIIArt.java
@@ -1,2 +1,48 @@
 public class ASCIIArt {
+
+    public static void printHangman(int strikes){
+	String gallows ="";
+
+	switch(strikes){
+	    case 0:
+		// set gallows string
+		break;
+	    case 1:
+		// set gallows string
+		break;
+	    case 2:
+		// set gallows string
+		break;
+	    case 3:
+		// set gallows string
+		break;
+	    case 4:
+		// set gallows string
+		break;
+	    case 5:
+		// set gallows string
+		break;
+	    case 7:
+		//set gallows string
+		break;
+	    default:
+		System.out.println("Unknown error. Default value reached in switch statement");
+	}
+    }
+
 }
+
+
+
+
+/*
+Notes
+-----
+0: Lines 0-10
+1: Lines 11-21
+2: Lines 22-32
+3: Lines 33-43
+4: Lines 44-54
+5: Lines 55-65
+6: Lines 66-76
+ */
