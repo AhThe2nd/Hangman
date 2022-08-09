ae0cbbec043184a3547d1844ac5fc72808314212
Andrew Howell <andrew.t.howell@gmail.com>
2022-08-09 20:53:28 -0230

    - Removed unnecessary calls in main method and cleaned up code  - Added some comments to clarify what was happening

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,5 @@
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
+
 
 public class Game {
     public static void main(String[] args) {
@@ -17,14 +15,26 @@ public class Game {
 
 	    // Store game word as a char array and progress as a char array
 	    System.out.println(word);
-	    char[] wordArray = word.toCharArray();
-	    char[] progressArray = "*".repeat(Math.max(0, word.length())).toCharArray();
+	    String[] wordArray = word.split("");
+	    String[] progressArray = "*".repeat(Math.max(0, word.length())).split("");
 
 	    // Print both arrays
 	    System.out.println("Word chosen as array: " + Arrays.toString(wordArray));
 	    System.out.println("Progress as array: " + Arrays.toString(progressArray));
 
-	    // Look into using an ArrayList<Pair> where the Pair<Character, Boolean>
+
+	    // Get a guess from a player
+	    String guess = Display.promptGuess();
+
+	    // Check if the guess is in the word
+	    for (int i = 0; i < wordArray.length; i++){
+		if (guess.equals(wordArray[i])){
+		    progressArray[i] = guess;
+		}
+	    }
+	    System.out.println(Arrays.toString(progressArray));
+
+
 
 	}
     }
